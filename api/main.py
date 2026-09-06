c17e30fe820231199f3a772673c711bfc5b341fa
Lorenzo <lorenzo@lrzodev.xyz>
2026-09-06 02:54:20 +0100

    Remove boilerplate code

--- api/main.py
+++ api/main.py
@@ -1,5 +1,2 @@
 def main() -> None:
     print("API says hello")
-
-if __name__ == "__main__":
-    main()
--- database/main.py
+++ database/main.py
@@ -1,5 +1,2 @@
 def main() -> None:
     print("Database says hello")
-
-if __name__ == "__main__":
-    main()
