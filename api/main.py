a3349c9feec4d2dedc795bdba4ae7da92717e602
Lorenzo <lorenzo@lrzodev.xyz>
2026-09-06 02:47:11 +0100

    Initial commit

---
+++ api/main.py
@@ -0,0 +1,5 @@
+def main() -> None:
+    print("API says hello")
+
+if __name__ == "__main__":
+    main()
---
+++ database/main.py
@@ -0,0 +1,5 @@
+def main() -> None:
+    print("Database says hello")
+
+if __name__ == "__main__":
+    main()
